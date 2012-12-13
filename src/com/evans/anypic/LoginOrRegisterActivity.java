5489242294e0aa3fe3470aa6414542dc5cb058dc
Michael Evans <michaelcevans@me.com>
2012-12-13 00:54:39 -0500

    you can now come back here

--- src/com/evans/anypic/LoginOrRegisterActivity.java
+++ src/com/evans/anypic/LoginOrRegisterActivity.java
@@ -1,18 +1,18 @@
 package com.evans.anypic;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 import com.parse.Parse;
 import com.parse.ParseUser;
 import com.parse.PushService;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class LoginOrRegisterActivity extends Activity {
 
@@ -20,6 +20,15 @@ public class LoginOrRegisterActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_or_register);
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//		if(cm.getAllNetworkInfo() != null && cm.getAllNetworkInfo().length > 0){
+//			if(!cm.getAllNetworkInfo()[0].isConnected()){
+//				Intent intent = new Intent(LoginOrRegisterActivity.this, StreamActivity.class);
+//				Log.d("CM", "CM " + cm.getAllNetworkInfo()[0]);
+//			    startActivity(intent);
+//			    //finish();
+//			}
+//		}
 		
 		Parse.initialize(this, "BII77Lso2wqjLSThScrTLqUTq6DQ8hhPMxX9C6sn", "0wRgjMrQNw4HPCWzwDrlAKytmwTy50luntP5o960");
 		PushService.subscribe(this, "", LoginOrRegisterActivity.class);
@@ -40,7 +49,6 @@ public class LoginOrRegisterActivity extends Activity {
 			public void onClick(View view) {
 				Intent intent = new Intent(LoginOrRegisterActivity.this, LoginActivity.class);
 			    startActivity(intent);
-			    finish();
 			}
 		});
 		Button goToRegister = (Button) findViewById(R.id.button2);
@@ -49,17 +57,16 @@ public class LoginOrRegisterActivity extends Activity {
 			public void onClick(View view) {
 				Intent intent = new Intent(LoginOrRegisterActivity.this, RegisterActivity.class);
 			    startActivity(intent);
-			    finish();
 			}
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_login_or_register, menu);
-		return true;
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.activity_login_or_register, menu);
+//		return true;
+//	}
 
 	
 
